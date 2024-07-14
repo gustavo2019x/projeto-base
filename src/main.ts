8bb3ea439c6b59b0aee6e1546d451b6b8e7dbfd8
Gustavo Neri <gnb.neri@gmail.com>
2024-07-14 14:37:27 -0300

    alow3

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
-import { App } from 'vue'
+//import { App } from 'vue'
 import './style.css'
 //import Inicio from './Inicio.vue'
 //createApp(Inicio).mount('#app')
 
 
 // export individual components
-import HeaderPadrao from './components/HeaderPadrao.vue';
+//import HeaderPadrao from './components/HeaderPadrao.vue';
 
-export { HeaderPadrao };
+//export { HeaderPadrao };
 
 // export the entire library as a plugin, so it can be installed via app.use()
 // comment this out if you don't want to install the entire library
 
-export default {
-  install: (app: App) => {
-    app.component('HeaderPadrao', HeaderPadrao);
-  },
-};
+// export default {
+//   install: (app: App) => {
+//     app.component('HeaderPadrao', HeaderPadrao);
+//   },
+// };
+export { default as HeaderPadrao } from './components/HeaderPadrao.vue';
